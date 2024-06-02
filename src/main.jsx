1f7a9d17da403bef182953c408966b4e54ea5f6e
Pat ODay <poday@outdoors.org>
2024-06-02 17:13:40 -0400

    Imported Provider from react-redux
    imported Provider and wrapped the <App /> in the <Provider>

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Provider } from 'react-redux';
+import App from './App'
+import {store} from './store/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
